f45ea18e6425309431af05cff2da14539973f686
Musa A. Musazada <musaam@code.local>
2021-12-06 19:03:25 +0400

    DbSets added

--- TASK-06-12-21/TASK-06-12-21/DAL/AppDbContext.cs
+++ TASK-06-12-21/TASK-06-12-21/DAL/AppDbContext.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TASK_06_12_21.Models;
 
 namespace TASK_06_12_21.DAL
 {
@@ -12,5 +13,19 @@ namespace TASK_06_12_21.DAL
         {
 
         }
+        public DbSet<Users> Users { get; set;}
+        public DbSet<Baskets> Baskets { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Roles> Roles { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Favorites> Favorites{ get; set; }
+
+
+
     }
 }
